5f97204511b981c3a045a21fffa35969a4897761
justanotheratom <sanketpatel.1805@gmail.com>
2023-08-28 13:22:13 -0700

    Initial Commit

---
+++ ShouldIEat/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ShouldIEat
+//
+//  Created by sanket patel on 8/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShouldIEat/ShouldIEatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShouldIEatApp.swift
+//  ShouldIEat
+//
+//  Created by sanket patel on 8/28/23.
+//
+
+import SwiftUI
+
+@main
+struct ShouldIEatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
