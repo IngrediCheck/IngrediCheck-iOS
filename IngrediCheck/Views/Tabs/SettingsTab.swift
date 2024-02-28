95db9023129b025eb6ae0b7365dfb65e462c9199
justanotheratom <sanketpatel.1805@gmail.com>
2024-02-28 08:54:53 -0800

    Minor - Show app version in settings tab

--- IngrediCheck/Views/Tabs/SettingsTab.swift
+++ IngrediCheck/Views/Tabs/SettingsTab.swift
@@ -5,17 +5,39 @@ struct SettingsTab: View {
     var body: some View {
         @Bindable var userPreferences = userPreferences
         NavigationStack {
-            Form {
-                Section {
-                    Picker("OCR Engine", selection: $userPreferences.ocrModel) {
-                        Text("iOS").tag(OcrModel.iOSBuiltIn)
-                        Text("Google").tag(OcrModel.googleMLKit)
+            VStack {
+                Form {
+                    Section {
+                        Picker("OCR Engine", selection: $userPreferences.ocrModel) {
+                            Text("iOS").tag(OcrModel.iOSBuiltIn)
+                            Text("Google").tag(OcrModel.googleMLKit)
+                        }
                     }
                 }
+                Text("App Version \(appVersion).(\(buildNumber))")
+                    .font(.footnote)
+                    .padding(.bottom, 10)
+                Divider()
+                    .padding(.bottom, 5)
             }
             .navigationBarTitleDisplayMode(.inline)
             .navigationTitle("SETTINGS")
         }
+    }
+    
+    
+    var appVersion: String {
+        guard let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String else {
+            return "0.0"
+        }
+        return version
+    }
+    
+    var buildNumber: String {
+        guard let buildNumber = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String else {
+            return "00"
+        }
+        return buildNumber
     }
 }
 
