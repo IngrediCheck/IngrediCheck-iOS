5f9cd97f975798ed71066d3db76c9f176b8621e4
justanotheratom <sanketpatel.1805@gmail.com>
2024-04-12 15:08:50 -0700

    UITweak - Analysis View when product has no images.

--- IngrediCheck/Views/ProductImagesView.swift
+++ IngrediCheck/Views/ProductImagesView.swift
@@ -11,21 +11,25 @@ struct ProductImagesView: View {
 
     var body: some View {
         VStack(spacing: 15) {
-            TabView(selection: $currentTabViewIndex.animation()) {
-                ForEach(0 ... images.count, id:\.self) { index in
-                    if index == images.count {
-                        uploadPhotoButton
-                    } else {
-                        HeaderImage(imageLocation: images[index])
-                            .frame(width: UIScreen.main.bounds.width - 110)
+            if images.count != 0 {
+                TabView(selection: $currentTabViewIndex.animation()) {
+                    ForEach(0 ... images.count, id:\.self) { index in
+                        if index == images.count {
+                            uploadPhotoButton
+                        } else {
+                            HeaderImage(imageLocation: images[index])
+                                .frame(width: UIScreen.main.bounds.width - 110)
+                        }
                     }
                 }
-            }
-            .background(.paletteBackground)
-            .frame(height: (UIScreen.main.bounds.width - 110) * (4/3))
-            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                .background(.paletteBackground)
+                .frame(height: (UIScreen.main.bounds.width - 110) * (4/3))
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
 
-            Fancy3DotsIndexView(numberOfPages: images.count + 1, currentIndex: currentTabViewIndex)
+                Fancy3DotsIndexView(numberOfPages: images.count + 1, currentIndex: currentTabViewIndex)
+            } else {
+                uploadPhotoButton
+            }
         }
     }
     
@@ -38,14 +42,14 @@ struct ProductImagesView: View {
                     .font(.largeTitle)
                     .padding()
                 Text("Upload a photo")
-                    .foregroundStyle(.paletteAccent)
+                    .foregroundStyle(.primary100)
                     .font(.headline)
             }
             .frame(width: UIScreen.main.bounds.width / 2)
             .frame(height: UIScreen.main.bounds.width / 2)
             .background {
                 RoundedRectangle(cornerRadius: 5)
-                    .fill(.paletteAccent.opacity(0.20))
+                    .fill(.primary50)
             }
         })
     }
