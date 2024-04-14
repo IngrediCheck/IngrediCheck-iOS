fc4dd34cb821cdc156f8c67ca982b117b53281a4
justanotheratom <sanketpatel.1805@gmail.com>
2024-04-14 11:23:46 -0700

    Splash Screen - Try to make transition work (does not work)

--- IngrediCheck/IngrediCheckApp.swift
+++ IngrediCheck/IngrediCheckApp.swift
@@ -11,33 +11,29 @@ struct IngrediCheckApp: App {
 
     var body: some Scene {
         WindowGroup {
-            if let _ = authController.authEvent {
-                if let _ = authController.session {
-                    LoggedInRootView()
-                        .environment(webService)
-                        .environment(userPreferences)
-                        .environment(appState)
-                        .environment(dietaryPreferences)
-                        .tint(.paletteAccent)
-                } else {
-                    Text("Sign-in failed")
-                }
-            } else {
-                SplashScreen()
+            Splash {
+                Image("SplashScreen")
+                    .resizable()
+                    .scaledToFill()
                     .onAppear {
-                        DispatchQueue.global().asyncAfter(deadline: .now() + 0.5) {
-                            authController.initialize()
-                        }
+                        authController.initialize()
                     }
+            } content: {
+                if let _ = authController.authEvent {
+                    if let _ = authController.session {
+                        LoggedInRootView()
+                            .environment(webService)
+                            .environment(userPreferences)
+                            .environment(appState)
+                            .environment(dietaryPreferences)
+                            .tint(.paletteAccent)
+                    } else {
+                        Text("Sign-in failed")
+                    }
+                } else {
+                    ProgressView()
+                }
             }
         }
-    }
-}
-
-struct SplashScreen: View {
-    var body: some View {
-        Image("SplashScreen")
-            .resizable()
-            .scaledToFill()
     }
 }
---
+++ IngrediCheck/Views/Lib/Splash.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+
+struct Splash<Splash: View, Content: View>: View {
+
+    var splashDuration: CGFloat = 1.0
+    var splash: Splash
+    var content: Content
+
+    @State var showingSplash: Bool = true
+
+    init(@ViewBuilder splash: () -> Splash,
+         @ViewBuilder content: () -> Content) {
+        self.splash = splash()
+        self.content = content()
+    }
+
+    var body: some View {
+        Group {
+            if showingSplash {
+                splash
+                    .onAppear { scheduleHideSplash() }
+                    .transition(.opacity.animation(.default))
+            } else {
+                content
+                    .transition(.opacity.animation(.default))
+            }
+        }
+    }
+
+    func scheduleHideSplash() {
+        DispatchQueue.main
+            .asyncAfter(deadline: .now() + Double(splashDuration)) {
+                withAnimation {
+                    showingSplash = false
+                }
+            }
+    }
+}
