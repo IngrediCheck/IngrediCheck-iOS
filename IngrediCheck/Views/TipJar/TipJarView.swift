ea84ca642ac481fee557d016d26337accb185766
gunjanAtEightinity <gunjan.haldar@eightinity.in>
2025-07-23 17:57:04 +0530

    Added TipJar
    Added TipJar

---
+++ IngrediCheck/Views/TipJar/TipJarView.swift
@@ -0,0 +1,80 @@
+//
+//  TipJarView.swift
+//  IngrediCheck
+//
+//  Created by Gunjan Haider on 23/07/25.
+//
+
+import SwiftUI
+
+struct TipJarView: View {
+    
+    @StateObject var vm = TipJarViewModel()
+    
+    var body: some View {
+        ZStack {
+            
+            Color("PaletteAccent").opacity(0.15).ignoresSafeArea()
+            
+            VStack {
+                Text("Support IngrediCheck")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding(.bottom, 12)
+                
+                Text("If you're enjoying IngrediCheck and would like to support its development, consider leaving a tip. Your support helps keep IngrediCheck free and enables ongoing development!")
+                    .font(.subheadline)
+                    .fontWeight(.medium)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal)
+                
+                Spacer()
+                
+                ForEach(vm.productsArr) { product in
+                    Button {
+                        Task {
+                            await vm.purchase(product)
+                        }
+                    } label: {
+                        TipCard(description: product.description, price: product.displayPrice)
+                    }
+
+                    
+                }
+                
+                Spacer()
+            }
+            .padding(.horizontal)
+        }
+    }
+}
+
+struct TipCard: View {
+    let description: String
+    let price: String
+
+    var body: some View {
+            HStack {
+                Text(description)
+                Spacer()
+                Text(price)
+            }
+            .foregroundStyle(.black)
+            .fontWeight(.medium)
+            .padding()
+            .padding(.vertical, 2)
+            .background(Color.white)
+            .cornerRadius(10)
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color.gray, lineWidth: 0.5)
+            )
+            .padding(.horizontal)
+            .padding(.vertical, 4)
+    }
+}
+
+
+#Preview {
+    TipJarView()
+}
---
+++ IngrediCheck/Views/TipJar/TipJarViewModel.swift
@@ -0,0 +1,75 @@
+//
+//  TipJarViewModel.swift
+//  IngrediCheck
+//
+//  Created by Gunjan Haider on 23/07/25.
+//
+
+import Foundation
+import StoreKit
+
+@MainActor
+class TipJarViewModel: ObservableObject {
+    @Published var productsArr = [Product]()
+    
+    init() {
+        Task {
+            await retriveProducts()
+            
+            Task.detached(priority: .background) {
+                for await result in Transaction.updates {
+                    do {
+                        let transaction = try result.payloadValue
+                        if transaction.revocationDate == nil {
+                            print("🔁 Transaction update received: \(transaction.productID)")
+                            await transaction.finish()
+                        }
+                    } catch {
+                        print("⚠️ Failed to handle transaction update: \(error)")
+                    }
+                }
+            }
+        }
+    }
+    
+    func retriveProducts() async {
+        do {
+            let products = try await Product.products(for: tipJarIdentifiers).sorted(by: { $0.price < $1.price })
+            productsArr = products
+        } catch {
+            print("Error while fetching products from connect file: \(error)")
+        }
+    }
+    
+    func purchase(_ product: Product) async {
+        do {
+            let result = try await product.purchase()
+            
+            switch result {
+                
+            case .success(let verification):
+                switch verification {
+                    
+                case .verified(let transaction):
+                    print("Purchase Successful: \(transaction.productID)")
+                    await transaction.finish()
+                    
+                case .unverified(_, let error):
+                    print("Unverified Purchase: \(error.localizedDescription)")
+                }
+                
+            case .userCancelled:
+                print("Purchase cancelled by the user.")
+                
+            case .pending:
+                print("Purchase is pending.")
+                
+            @unknown default:
+                print("Unknown purchase result.")
+            }
+            
+        } catch {
+            print("Failed to purchase the product: \(error.localizedDescription)")
+        }
+    }
+}
