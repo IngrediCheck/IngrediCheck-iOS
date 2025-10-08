a7793fc210350edf8e3b2aec5aa8ed5a0f7cc587
gunjanAtEightinity <gunjan.haldar@eightinity.in>
2025-10-08 15:23:37 +0530

    Update ProfileCard UI and add Primary700 color asset
    Refactored ProfileCard to support completed and incomplete profile states with distinct UI. Added Primary700 color asset for use in components. Simplified Temp3 view to demonstrate gradient and shadow styling.

--- IngrediCheckPreview/Utilities/Temp3.swift
+++ IngrediCheckPreview/Utilities/Temp3.swift
@@ -9,57 +9,18 @@ import SwiftUI
 
 struct Temp3: View {
     
-    @State private var cards: [Card] = [
-        Card(text: "Blue", color: .blue), Card(text: "Red", color: .red), Card(text: "Yellow", color: .yellow),
-        Card(text: "Green", color: .green), Card(text: "Pink", color: .pink), Card(text: "Brown", color: .brown)
-    ]
-    
-    @State var chips: [ChipsModel] = [
-        ChipsModel(name: "High Protein", icon: "chicken"),
-        ChipsModel(name: "Low Carb", icon: "cucumber"),
-        ChipsModel(name: "Low Fat", icon: "avacardo"),
-        ChipsModel(name: "Balanced Marcos", icon: "weight-machine"),
-        ChipsModel(name: "High Protein", icon: "chicken"),
-        ChipsModel(name: "Low Carb", icon: "cucumber"),
-        ChipsModel(name: "Low Fat", icon: "avacardo"),
-        ChipsModel(name: "Balanced Marcos", icon: "weight-machine")
-    ]
-    
     var body: some View {
-        ZStack {
-            
-            VStack(alignment: .leading) {
-                Text("Macronutrient Goals")
-                    .font(.system(size: 20, weight: .regular))
-                
-                Text("Do you want to balance your proteins, carbs, and fats-or focus on one?")
-                    .font(.system(size: 12, weight: .regular))
-                    .opacity(0.8)
-                
-                FlowLayout(horizontalSpacing: 4, verticalSpacing: 8) {
-                    ForEach(chips, id: \.id) { chip in
-                        HStack {
-                            Image(chip.icon)
-                                .resizable()
-                                .frame(width: 24, height: 24)
-                            
-                            Text(chip.name)
-                                .font(.system(size: 12, weight: .regular))
-                        }
-                        .padding(.horizontal, 12)
-                        .padding(.vertical, 8)
-                        .background(Capsule().foregroundStyle(Color(hex: "#ECECEC")))
-                    }
-                }
-            }
-            .padding(.horizontal, 12)
-            .padding(.vertical, 20)
-            .background(Color(hex: "#D7D6D6"), in: RoundedRectangle(cornerRadius: 16))
-            
-        }
+        Circle()
+            .foregroundStyle(
+                .blue
+                .gradient
+                .shadow(.inner(color: .black, radius: 10, x: 0, y: 14))
+                .shadow(.inner(color: .black, radius: 10, x: 0, y: -14))
+            )
     }
 }
 
 #Preview {
     Temp3()
+        .padding()
 }
