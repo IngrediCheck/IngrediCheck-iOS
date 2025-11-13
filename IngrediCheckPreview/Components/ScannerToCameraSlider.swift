098a4ffa0984d762e74678ef4770ce5966255514
gunjanAtEightinity <gunjan.haldar@eightinity.in>
2025-11-13 12:59:43 +0530

    Add ScannerToCameraSlider scaffold and preview
    - Create new SwiftUI view component in IngrediCheckPreview/Components\n- Add basic placeholder content and preview provider\n- Sets up groundwork for scanner-to-camera slider interaction

---
+++ IngrediCheckPreview/Components/ScannerToCameraSlider.swift
@@ -0,0 +1,18 @@
+//
+//  ScannerToCameraSlider.swift
+//  IngrediCheckPreview
+//
+//  Created by Gunjan Haldar   on 13/11/25.
+//
+
+import SwiftUI
+
+struct ScannerToCameraSlider: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScannerToCameraSlider()
+}
