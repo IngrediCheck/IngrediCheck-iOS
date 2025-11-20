9161fe19e729f04e9a951de0accf9a57f656c645
gunjanAtEightinity <gunjan.haldar@eightinity.in>
2025-11-20 14:32:57 +0530

    Update green button styles for onboarding canvas

--- IngrediCheckPreview/Components/Buttons/GreenCircle.swift
+++ IngrediCheckPreview/Components/Buttons/GreenCircle.swift
@@ -20,20 +20,24 @@ struct GreenCircle: View {
             .padding(10)
             .background(
                 Capsule()
-                    .fill(
-                        .primary800
+                    .frame(width: circleSize, height: circleSize)
+                    .foregroundStyle(
+                        LinearGradient(
+                            colors: [Color(hex: "9DCF10"), Color(hex: "6B8E06")],
+                            startPoint: .top,
+                            endPoint: .bottom
+                        )
                         .shadow(
                             .drop(color: Color(hex: "C5C5C5").opacity(0.57), radius: 11, x: 0, y: 4)
                         )
                         .shadow(
-                            .inner(color: Color(hex: "DAFF67").opacity(0.25), radius: 7.3, x: 2, y: 9)
+                            .inner(color: Color(hex: "EDEDED").opacity(0.25), radius: 7.5, x: 2, y: 4)
                         )
                         .shadow(
-                            .inner(color: Color(hex: "A2D20C"), radius: 5.7, x: 0, y: 4)
+                            .inner(color: Color(hex: "72930A"), radius: 5.7, x: 0, y: 4)
                         )
                         
                     )
-                    .frame(width: circleSize, height: circleSize)
                     .rotationEffect(.degrees(17))
                     .overlay(
                         Circle()
